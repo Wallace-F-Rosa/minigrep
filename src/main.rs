1472790bd70418f17726c0eb103cb3d930024c17
Wallace Ferancini Rosa <wferancini@gmail.com>
2022-07-04 20:06:41 -0300

    feat(main): error message is colored red

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,15 @@ use std::env;
 use std::process;
 
 fn main() {
+    // println!("\x1b[93mError\x1b[0m");
+    // println!("\x1b[31mError\x1b[0m");
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("\x1b[31mProblem parsing arguments:\x1b[0m {}", err);
         process::exit(1);
     });
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("\x1b[31mApplication error:\x1b[0m {}", e);
         process::exit(1);
     }
 }
